901a9e6d806a3a4bc14f8f1ea48fa592ce861b8b
milkslapper <oscar.valenzuela69@gmail.com>
2019-08-30 18:09:33 -0700

    added post endpoint for register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+const Users = require("./auth-model")
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const newUser = req.body;
+  if (!newUser.username || !newUser.password) {
+    res.status(400).json({ message: "Please provide username and password." });
+  } else {
+    Users.add(newUser)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "There was an error registering the new user." });
+      });
+  }
 });
 
 router.post('/login', (req, res) => {
